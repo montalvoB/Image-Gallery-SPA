260466416d1f78ffc041ad3f31ea75044de2d14f
montalvoB <brandon.mont4@gmail.com>
2025-05-28 21:58:10 -0700

    server setup

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,14 +1,24 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { ValidRoutes } from "./shared/ValidRoutes";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
+const STATIC_DIR = process.env.STATIC_DIR || "public";
 
 const app = express();
+
+app.use(express.static(STATIC_DIR));
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
 });
+
+app.get(Object.values(ValidRoutes), (req: Request, res: Response) =>{
+    res.sendFile(path.resolve(STATIC_DIR, "index.html"));
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -6,20 +6,21 @@ import { fetchDataFromServer } from "./MockAppData.ts";
 import { useState } from "react";
 import { Routes, Route } from "react-router";
 import { MainLayout } from "./MainLayout.tsx";
+import { ValidRoutes } from "../../backend/src/shared/ValidRoutes.ts";
 
 function App() {
   const [imageData, _setImageData] = useState(fetchDataFromServer);
 
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
+      <Route path={ValidRoutes.HOME} element={<MainLayout />}>
         <Route index element={<AllImages imageData={imageData} />} />
         <Route
-          path="images/:imageId"
+          path={ValidRoutes.IMAGE_DETAILS}
           element={<ImageDetails imageData={imageData} />}
         />
-        <Route path="upload" element={<UploadPage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path={ValidRoutes.UPLOAD} element={<UploadPage />} />
+        <Route path={ValidRoutes.LOGIN} element={<LoginPage />} />
       </Route>
     </Routes>
   );
